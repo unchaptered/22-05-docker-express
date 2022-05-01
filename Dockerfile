cd1782ac6f95c1fc138e8a57a263135bf3c1b5dd
Unchaptered <workstation19961002@gmail.com>
2022-05-02 00:07:11 +0900

    ADD Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Base Image
+FROM node:14
+
+WORKDIR /usr/src/app
+
+COPY package.json ./
+
+RUN npm install
+
+COPY ./ ./
+
+CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -1,16 +1,12 @@
+
 const express = require('express');
 
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
-
-
 const app = express();
 
-app.get('/', (req, res) => {
-
-    res.send("Hello World");
-});
+app.get('/', (req, res) => res.send("짜장면"));
 
 app.listen(PORT, HOST);
 
